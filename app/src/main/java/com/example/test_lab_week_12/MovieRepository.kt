ca01186bcc2abae96491e9352b35601c81538720
ifBeans <fabiandustin27@gmail.com>
2025-12-10 23:54:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -0,0 +1,34 @@
+package com.example.test_lab_week_13
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.test_lab_week_13.api.MovieService
+import com.example.test_lab_week_13.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class MovieRepository(private val movieService: MovieService) {
+    private val apiKey = "8401cc28ce7ae70d9c3d69f61ff27e1d"
+    // LiveData that contains a list of movies
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+    // LiveData that contains an error message
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+
+    // fetch movies from the API
+    // this function returns a Flow of Movie objects
+    // a Flow is a type of coroutine that can emit multiple values
+    // for more info, see: https://kotlinlang.org/docs/flow.html#flows
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            // emit the list of popular movies from the API
+            emit(movieService.getPopularMovies(apiKey).results)
+            // use Dispatchers.IO to run this coroutine on a shared pool of threads
+        }.flowOn(Dispatchers.IO)
+    }
+}
