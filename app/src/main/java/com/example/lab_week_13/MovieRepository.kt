109f56b9eedff6dd7855916b726b1c2608d8cc73
ifBeans <fabiandustin27@gmail.com>
2025-12-11 01:35:20 +0700

    Commit No. 2 – update to Room

--- app/src/main/java/com/example/lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/lab_week_13/MovieRepository.kt
@@ -3,13 +3,15 @@ package com.example.lab_week_13
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.lab_week_13.api.MovieService
+import com.example.lab_week_13.database.MovieDao
+import com.example.lab_week_13.database.MovieDatabase
 import com.example.lab_week_13.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class MovieRepository(private val movieService: MovieService) {
+class MovieRepository(private val movieService: MovieService, private val movieDatabase: MovieDatabase) {
     private val apiKey = "8401cc28ce7ae70d9c3d69f61ff27e1d"
     // LiveData that contains a list of movies
     private val movieLiveData = MutableLiveData<List<Movie>>()
@@ -26,9 +28,22 @@ class MovieRepository(private val movieService: MovieService) {
     // for more info, see: https://kotlinlang.org/docs/flow.html#flows
     fun fetchMovies(): Flow<List<Movie>> {
         return flow {
-            // emit the list of popular movies from the API
-            emit(movieService.getPopularMovies(apiKey).results)
-            // use Dispatchers.IO to run this coroutine on a shared pool of threads
+            // Check if there are movies saved in the database
+            val movieDao: MovieDao = movieDatabase.movieDao()
+            val savedMovies = movieDao.getMovies()
+            // If there are no movies saved in the database,
+            // fetch the list of popular movies from the API
+            if(savedMovies.isEmpty()) {
+                val movies = movieService.getPopularMovies(apiKey).results
+                // save the list of popular movies to the database
+                movieDao.addMovies(movies)
+                // emit the list of popular movies from the API
+                emit(movies)
+            } else {
+                // If there are movies saved in the database,
+                // emit the list of saved movies from the database
+                emit(savedMovies)
+            }
         }.flowOn(Dispatchers.IO)
     }
 }
